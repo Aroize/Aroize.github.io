4f5608c9f8bba4455f509ab8827128f047357e95
Ilia <i.ilmenskiy@corp.vk.com>
2021-03-24 20:44:20 +0300

    implemented getting data for current city geo

--- js/geo-initial.js
+++ js/geo-initial.js
@@ -1,218 +1,206 @@
-function capitalizeFirstLetter(string) {
-    return string.slice(0, 1) + string.slice(1);
+window.onload = function () {
+
+    const currentCityGeoCallback = function (lat, lon) {
+        const city = {
+            "city_name": null,
+            "lat": lat,
+            "lon": lon
+        }
+        const currentCityWeatherCallback = function (weather) {
+            showCurrentCityGeo(city, weather);
+        }
+
+        requestCityWeather(city, currentCityWeatherCallback)
+    }
+    getCurrentGeo(currentCityGeoCallback);
+
+    const storedCitiesCallback = function (cities) {
+        cities.forEach(city => {
+            const cityWeatherCallback = function (weather) {
+                appendCityToFavourites(city, weather);
+            }
+            requestCityWeather(city, cityWeatherCallback);
+        });
+    }
+
+    getStorageCities(storedCitiesCallback);
 }
 
-class Worker {
 
-    apiManager = new ApiManager();
+/**
+ * geo utils
+ */
 
-    geolocationUtil = new GeolocationUtils();
-
-    viewManager = new ViewManager();
-
-    latLonCallback = function (lat, lon) {
-        mainWorker.apiManager.requestWeather(
-            Math.floor(lat),
-            Math.floor(lon),
-            mainWorker.weatherRequestMainCallback
-        );
-    }
-    defaultCityCallback = function () {
-        console.log("Should request default city");
+function getCurrentGeo(callback) {
+    const geolocation = navigator.geolocation;
+    const positionCallback = function (position) {
+        callback(position.coords.latitude, position.coords.longitude);
     }
 
-    weatherRequestCallback = function (weather) {
-        console.log(weather);
+    const positionErrorCallback = function (positionError) {
+        // Hardcoded SP coordinates
+        callback(60., 31.);
     }
 
-    weatherRequestMainCallback = function (weather) {
-        console.log(weather);
-        mainWorker.viewManager.showMainCityWeather(weather);
-    }
+    geolocation.getCurrentPosition(positionCallback, positionErrorCallback, null);
+}
 
-    updateCurrentCity() {
-        this.geolocationUtil.initialize(this.latLonCallback, this.defaultCityCallback);
-    }
+/**
+ * api utils
+ */
 
-    run() {
-        this.updateCurrentCity();
+const apiKey = "16cef8b09402035e00967f9f98573581"
+
+function formatRequestWithGeoCoords(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=ru`
+}
+
+function formatWeatherIcon(iconId) {
+    return `http://openweathermap.org/img/wn/${iconId}@4x.png`
+}
+
+function requestCityWeather(city, requestCallback) {
+    // TODO(): add lat and lon to check
+    if (city["city_name"] == null) {
+        requestCityWeatherByGeoCoords(city, requestCallback);
+    } else {
+        requestCityWeatherByCityName(city, requestCallback);
     }
 }
 
-class ApiManager {
-    weatherManager = new WeatherApiManager();
-    searchManager = new SearchApiManager();
-
-    requestWeather(lat, lon, callback) {
-        return this.weatherManager.requestWeather(lat, lon, callback);
-    }
-}
-
-class WeatherApiManager {
-
-    weatherKey = "4e45331b-af93-424b-96d5-50c2f4e72802";
-
-    createHttpRequest(lat, lon) {
-        return `https://api.weather.yandex.ru/v2/forecast?lat=${lat}&lon=${lon}&lang=ru_RU`;
-    }
-
-    requestWeather(lat, lon, callback) {
-        const wholeResponseCallback = function (hugeResponseCallback) {
-            if (hugeResponseCallback == null) {
-                callback(null);
+function requestCityWeatherByGeoCoords(city, callback) {
+    const xhr = new XMLHttpRequest();
+    const requestUrl = formatRequestWithGeoCoords(city["lat"], city["lon"]);
+    xhr.open("GET", requestUrl, true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                console.log("Bruh moment");
             } else {
-                const factWeather = hugeResponseCallback['fact'];
-                const iconName = factWeather['icon'];
-                const iconUrl = `https://yastatic.net/weather/i/icons/blueye/color/svg/${iconName}.svg`
-                const weatherBlockDto = {
-                    "wind": {
-                        "wind_dir": factWeather['wind_dir'],
-                        "wind_speed": factWeather['wind_speed']
-                    },
-                    "condition": factWeather['condition'],
-                    "pressure": factWeather['pressure_pa'],
-                    "humidity": factWeather['humidity'],
-                    "coordinates": {
-                        'lat': lat,
-                        'lon': lon
-                    },
-                    "icon": iconUrl,
-                    "temp": factWeather['temp'],
-                    "city": hugeResponseCallback['geo_object']['province']['name']
-                };
-                callback(weatherBlockDto);
+                const jsonResponse = JSON.parse(xhr.responseText);
+                city['city_name'] = jsonResponse['name'];
+                const weather = parseWeather(jsonResponse);
+                callback(weather);
             }
         }
-        this.requestCityWeather(lat, lon, wholeResponseCallback);
     }
+    xhr.send();
+}
 
-    requestCityWeather(lat, lon, callback) {
-        mainWorker.viewManager.showProgress();
-        const requestUrl = this.createHttpRequest(lat, lon);
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", requestUrl, true);
-        xhr.setRequestHeader("X-Yandex-API-Key", this.weatherKey);
-        xhr.onload = function (e) {
-            if (xhr.readyState === 4) {
-                mainWorker.viewManager.hideProgress();
-                if (xhr.status !== 200) {
-                    callback(null);
-                } else {
-                    callback(JSON.parse(xhr.responseText));
-                }
-            }
-        }
-        xhr.send()
+function requestCityWeatherByCityName() {
+
+}
+
+function parseWeather(weatherJson) {
+    const weatherMain = weatherJson['main'];
+    const weatherAddon = weatherJson['weather'][0];
+    const wind = weatherJson['wind']
+    return {
+        'humidity': weatherMain['humidity'],
+        'temperature': convertKelvinToCelsius(weatherMain['temp']),
+        'pressure': weatherMain['pressure'],
+        'cloudiness': weatherAddon['description'],
+        'icon_url': formatWeatherIcon(weatherAddon['icon']),
+        'wind_direction': extractWindDirectionFromDegrees(wind['deg']),
+        'wind_speed': wind['speed']
     }
 }
 
-class SearchApiManager {
-    searchKey = "46edd134-b17f-43b6-8a70-da5c9841aba6";
+function convertKelvinToCelsius(kelvin) {
+    return Math.floor(kelvin - 273);
 }
 
-class GeolocationUtils {
-    initialize(successCallback, failureCallback) {
-        const geolocation = navigator.geolocation;
-        const positionCallback = function (position) {
-            console.log(position);
-            successCallback(position.coords.latitude, position.coords.longitude);
-        }
-        const positionErrorCallback = function (positionError) {
-            console.log(positionError);
-            failureCallback();
-        }
-        geolocation.getCurrentPosition(positionCallback, positionErrorCallback, null);
-    }
-}
-
-class ViewManager {
-    showMainCityWeather(weather) {
-        const cityName = document.getElementById("geolocation-city-name");
-        const cityIcon = document.getElementById("geolocation-city-icon");
-        const cityTemperature = document.getElementById("geolocation-temperature");
-
-        const geoInfo = document.getElementsByClassName("info-by-geolocation")[0];
-
-        cityName.innerHTML = weather['city'];
-        cityIcon.src = weather['icon'];
-        cityTemperature.innerHTML = `${weather['temp']}°C`;
-
-
-        const geoInfoList = geoInfo.children[0].children;
-        for (let i = 0; i < geoInfoList.length; ++i) {
-            const listItem = geoInfoList[i];
-            const geoField = listItem.children[0];
-            switch (i) {
-                case 0:
-                    const wind = weather['wind'];
-                    geoField.innerHTML = `${wind['wind_speed']}m/s, ${this.processWindDirection(wind['wind_dir'])}`;
-                    break;
-                case 1:
-                    const condition = weather['condition'];
-                    geoField.innerHTML = capitalizeFirstLetter(condition);
-                    break;
-                case 2:
-                    const pressure = weather['pressure'];
-                    geoField.innerHTML = `${pressure} hpa`
-                    break;
-                case 3:
-                    const humidity = weather['humidity'];
-                    geoField.innerHTML = `${humidity}%`
-                    break;
-                case 4:
-                    const coordinates = weather['coordinates'];
-                    geoField.innerHTML = `[${coordinates['lat']}, ${coordinates['lon']}]`
-                    break;
-            }
-        }
-
-    }
-
-    processWindDirection(direction) {
-        let expandedDirection = "nowhere";
-        switch (direction) {
-            case "nw":
-                expandedDirection = "Northwest"
-                break;
-            case "n":
-                expandedDirection = "Northern";
-                break;
-            case "ne":
-                expandedDirection = "Northeastern";
-                break;
-            case "se":
-                expandedDirection = "Southeast";
-                break;
-            case "s":
-                expandedDirection = "Southern";
-                break;
-            case "sw":
-                expandedDirection = "Southwest";
-                break;
-            case "w":
-                expandedDirection = "Western";
-                break;
-            case "с":
-                expandedDirection = "Calm";
-                break;
-        }
-        return expandedDirection;
-    }
-
-    showProgress() {
-        const loadingContainer = document.getElementById("loading-view");
-        loadingContainer.style.height = "100%";
-        loadingContainer.style.visibility = "visible";
-    }
-
-    hideProgress() {
-        setTimeout(function () {
-            const loadingContainer = document.getElementById("loading-view");
-            loadingContainer.style.height = "0";
-            loadingContainer.style.visibility = "hidden";
-        }, 500)
+function extractWindDirectionFromDegrees(deg) {
+    if (deg > 348.75 || deg < 11.25) {
+        return "North";
+    } else if (deg > 11.25 && deg < 33.75) {
+        return "North, Northeast";
+    } else if (deg > 33.75 && deg < 56.25) {
+        return "Northeast";
+    } else if (deg > 56.25 && deg < 78.75) {
+        return "East, Northeast";
+    } else if (deg > 78.75 && deg < 101.25) {
+        return "East";
+    } else if (deg > 101.25 && deg < 123.75) {
+        return "East, Southeast";
+    } else if (deg > 123.75 && deg < 146.25) {
+        return "Southeast";
+    } else if (deg > 146.25 && deg < 168.75) {
+        return "South, Southeast";
+    } else if (deg > 168.75 && deg < 191.25) {
+        return "South";
+    } else if (deg > 191.25 && deg < 213.75) {
+        return "South, Southwest";
+    } else if (deg > 213.75 && deg < 236.25) {
+        return "Southwest";
+    } else if (deg > 236.25 && deg < 258.75) {
+        return "West, Southwest";
+    } else if (deg > 258.75 && deg < 281.25) {
+        return "West"
+    } else if (deg > 281.25 && deg < 303.75) {
+        return "West, Northwest";
+    } else if (deg > 303.75 && deg < 326.25) {
+        return "Northwest";
+    } else {
+        return "North, Northwest";
     }
 }
 
-const mainWorker = new Worker();
-mainWorker.run();
+/**
+ * view utils
+ */
+
+function appendCityToFavourites(city, weather) {
+
+}
+
+function showCurrentCityGeo(city, weather) {
+    const cityName = document.getElementById("geolocation-city-name");
+    const cityIcon = document.getElementById("geolocation-city-icon");
+    const cityTemperature = document.getElementById("geolocation-temperature");
+
+    const geoInfo = document.getElementsByClassName("info-by-geolocation")[0];
+
+    cityName.innerHTML = city['city_name'];
+    cityIcon.src = weather['icon_url'];
+    cityTemperature.innerHTML = `${weather['temperature']}°C`;
+
+
+    const geoInfoList = geoInfo.children[0].children;
+    for (let i = 0; i < geoInfoList.length; ++i) {
+        const listItem = geoInfoList[i];
+        const geoField = listItem.children[0];
+        switch (i) {
+            case 0:
+                geoField.innerHTML = `${weather['wind_speed']}m/s, ${weather['wind_direction']}`;
+                break;
+            case 1:
+                const condition = weather['cloudiness'];
+                geoField.innerHTML = capitalizeFirstLetter(condition);
+                break;
+            case 2:
+                const pressure = weather['pressure'];
+                geoField.innerHTML = `${pressure} hpa`
+                break;
+            case 3:
+                const humidity = weather['humidity'];
+                geoField.innerHTML = `${humidity}%`
+                break;
+            case 4:
+                geoField.innerHTML = `[${Math.floor(city['lat'])}, ${Math.floor(city['lon'])}]`
+                break;
+        }
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+
+/**
+ * storage utils
+ */
+
+function getStorageCities() {
+    return [];
+}
